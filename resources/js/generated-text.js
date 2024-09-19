a65965900bc6fcbe4b88adf68ca1b9cba0f9e3a6
kuznikerik <erik.kuznik@gmail.com>
2024-09-19 13:28:38 +0200

    add confirmation modal & move button if widget has content

--- resources/js/generated-text.js
+++ resources/js/generated-text.js
@@ -1,44 +1,55 @@
 (function ($, Drupal, once, drupalSettings) {
   Drupal.behaviors.generatedText = {
-
     attach: function (context, settings) {
+      once("generatedText", ".generated-text-widget", context).forEach(
+        function (element) {
+          let $widget = $(element);
+          let $textarea = $widget.find("textarea");
+          const $fieldName = $widget.data("field-name");
+          const $fieldSettings = drupalSettings.iq_text_generator[$fieldName];
+          const $modal = $("#iq-text-generator-confirmation-modal");
+          const $button = $widget.find(".generated-text-button");
 
-      once('generatedText', '.generated-text-widget', context).forEach(function (element) {
-        let $widget = $(element);
-        let $textarea = $widget.find('textarea');
-        const $fieldName = $widget.data('field-name');
-        const $fieldSettings = drupalSettings.iq_text_generator[$fieldName];
+          // @todo if there is text in the textarea, show the button as icon left from the field label
 
-        // @todo if there is text in the textarea, show the button as icon left from the field label
+          $button.on("click", function (event) {
+            event.preventDefault();
+            $modal.modal("show");
+            $modal.find(".btn-primary").on("click", function () {
+              $modal.modal("hide");
+              $.ajax({
+                url: $fieldSettings.url,
+                method: "POST",
+                data: JSON.stringify($fieldSettings.inputs),
+                contentType: "application/json",
+                success: function (response) {
+                  if (response.text) {
+                    $textarea.text(response.text);
+                    $widget.find(".generated-text-button").hide();
+                  } else {
+                    alert("No text generated");
+                  }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                  // Handle any errors here.
+                  console.error(textStatus, errorThrown);
+                },
+              });
+            });
 
-        $widget.find('.generated-text-button').click(function (event) {
-          event.preventDefault();
+            // @todo add throbber or spinner to button and prevent further clicks
+          });
 
-          // @todo add throbber or spinner to button and prevent further clicks
-
-          $.ajax({
-            url: $fieldSettings.url,
-            method: 'POST',
-            data: JSON.stringify($fieldSettings.inputs),
-            contentType: 'application/json',
-            success: function (response) {
-              if (response.text) {
-                $textarea.text(response.text);
-                $widget.find('.generated-text-button').hide();
-              }
-              else {
-                alert('No text generated');
-              }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-              // Handle any errors here.
-              console.error(textStatus, errorThrown);
+          $(document).ready(function () {
+            if ($widget.hasClass("has-content")) {
+              const id = $widget.closest(".tabs__content").attr("id");
+              $button.insertAfter(
+                $widget.closest(".tabs").find(`.tabs__button[data-tab-toggle="${id}"] span`)
+              );
             }
           });
-        });
-      });
-
-    }
-
+        }
+      );
+    },
   };
 })(jQuery, Drupal, once, drupalSettings);
